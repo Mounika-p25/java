c881ce2e63107166e21df8531cb5e06d2490f9bf
Mounika-p25 <165769959+Mounika-p25@users.noreply.github.com>
2024-06-09 15:51:27 +0000

    datatype

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+class Datatypes
+{
+    public static void main (String args[])
+    {
+        byte b=13;
+        short s=244;
+        int i=7633;
+        long l=67387827;
+        float f=6.5f;
+        double d=654.3;
+        char c='f';
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
